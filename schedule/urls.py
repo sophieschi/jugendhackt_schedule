21d7bef24938276f677b876388a6e5b16675b818
Franziska Kunsmann <hi@kunsmann.eu>
2023-05-07 09:39:24 +0200

    add html fahrplan

--- schedule/urls.py
+++ schedule/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<slug:slug>.xml", views.schedule_xml, name="schedule_xml"),
+    path("schedule/<slug:slug>.xml", views.schedule_xml, name="schedule_xml"),
+    path("schedule/<slug:slug>.html", views.schedule_html, name="schedule_html"),
+    path("", views.index, name="index"),
 ]
--- schedule/views.py
+++ schedule/views.py
@@ -1,9 +1,45 @@
-from datetime import timedelta
+from datetime import datetime, timedelta
 
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 
 from .models import Event, Room, ScheduleEntry
+
+
+def index(request):
+    today = datetime.now().replace(hour=0, minute=0, second=0)
+
+    return render(
+        request,
+        "schedule/index.html",
+        context={
+            "events": Event.objects.all(),
+        },
+    )
+
+
+def schedule_html(request, slug):
+    event = get_object_or_404(Event, acronym=slug)
+
+    schedule = {}
+    for day in range(0, event.duration_days):
+        schedule[day] = {
+            "day": day + 1,
+            "start": event.start + timedelta(days=day),
+            "end": event.start + timedelta(days=day + 1),
+            "events": ScheduleEntry.objects.filter(
+                start__date=event.start + timedelta(days=day)
+            ).order_by("start"),
+        }
+
+    return render(
+        request,
+        "schedule/schedule.html",
+        context={
+            "event": event,
+            "schedule": schedule,
+        },
+    )
 
 
 def schedule_xml(request, slug):
@@ -22,7 +58,7 @@ def schedule_xml(request, slug):
         for room in Room.objects.filter(event=event):
             entries = ScheduleEntry.objects.filter(
                 room=room, start__date=event.start + timedelta(days=day)
-            )
+            ).order_by("start")
             if entries.count():
                 schedule[day]["rooms"][room] = {
                     "name": room.name,
